2c8244fe4bd7a4d828dc27490833bcfd48714cdc
Ezy93 <giordani001@gmail.com>
2022-03-08 16:27:07 +0100

    aggiunto funzionalita di input da utente

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
+//TODO FAR SPARIRE I NUMERI DENTRO I DIV E CONFRONTARLI CON QUELLI INSERITI DALL'UTENTE
+
 let numbersDisplay = document.querySelector("#numberDisplay");
 
 let randomNumbers = [];
 
+
+
+let userNumbers = [];
+
+
 let check = true;
+
 
 
 
@@ -18,10 +26,7 @@ let check = true;
 randomInt(5 , randomNumbers);
 console.log(randomInt(5 , randomNumbers));
 
-/* for(let i = 0; i < randomNumbers.length; i++){
-    numbersDisplay.innerHTML += randomNumbers[i];
-    
-}  */
+
 
 for(let i =0; i < randomNumbers.length; i++) {
 
@@ -30,12 +35,15 @@ for(let i =0; i < randomNumbers.length; i++) {
     divNumberContainer.classList.add("numberContainer" , "d-flex" , "justify-content-center" , "align-items-center" , "fs-1");
     divNumberContainer.style.width = `calc(100% / ${randomNumbers.length})`
     numbersDisplay.appendChild(divNumberContainer);
+    divNumberContainer.innerHTML = randomNumbers[i];
 
 }
 
+setTimeout(userInput , 10000)
 
 
 
+console.log(userNumbers)
 
 
 
@@ -53,7 +61,7 @@ for(let i =0; i < randomNumbers.length; i++) {
 function randomInt(howMuch,where){
     while(check){
         let randomInt = Math.floor(Math.random() * (howMuch - 1) + 1);
-        console.log(typeof randomInt)
+        console.log(randomInt)
         randomNumbers.push(randomInt);
 
         /* if(!randomNumbers.includes(randomInt)){
@@ -68,4 +76,14 @@ function randomInt(howMuch,where){
     
     return randomNumbers;
 }
+
+/* funzione che chiede cinque numeri all'utente con cinque prompt diversi e li inserisce in un array */
+function userInput(){
+    for(let i = 0; i < randomNumbers.length ; i++){
+        let userIntInput = parseInt(prompt("inserisci un numero che era mostrato precedentemente: ").trim());
+        userNumbers.push(userIntInput);
+    }
+};
+
+
 
